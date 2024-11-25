3a0bc4329a9b07ce361fd1a3db7a8e995d531cc6
PauloSameVibes <00001083720569Sp@al.educacao.sp.gov.br>
2024-11-25 16:03:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidade',
+    'O que é a terra do sol nascente?',
+    'Tanto Nippon quanto Nihon (que siginfica Japão), literalmente significam "origem do sol" e muitas vezes são traduzidos como a "Terra do Sol Nascente". Esta nomenclatura vem das missões do Império com a dinastia chinesa Sui e refere-se a posição a leste do Japão em relação à China.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tóquio'
+)
+
+criaCartao(
+    'História',
+    'O que é um samurai?',
+    'Um termo muito usado para se referir aos samurais é Bushi ( 武士 ), que significa literalmente "guerreiro". É a raiz da palavra Bushido ( 武士道 ), ou "Caminho do Guerreiro".'
+)
+
+criaCartao(
+    'Lingua Japonesa',
+    'Como se diz oi em Japones?',
+    'Oi em Japonês é Konnichiwa (Kon-ni-ti-wa)'
+)
